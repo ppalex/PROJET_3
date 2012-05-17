41bb65963ed0decb2abf9a5eef227beaf506363f
Simon Thibert <simon.thibert@student.uclouvain.be>
2012-05-17 13:19:47 +0200

    record

--- record.c
+++ record.c
@@ -34,44 +34,45 @@ int last_modification;
 
 int filesize=0;
 
-
+int a;
+int f;
 
 void sig_handler(int signum) {
 	signal(signum, SIG_IGN);
 	printf("\n***END***\n");
 	// Fermeture des fichiers
-	/*if(close(archive)==-1) {
+	if(close(a)==-1) {
 		perror("close");
 		exit(EXIT_FAILURE);
 	}
-	if(close(file)==-1) {
+	if(close(f)==-1) {
 		perror("close");
 		exit(EXIT_FAILURE);
-	}*/
+	}
 	exit(EXIT_FAILURE);
 }
 
-char* read_file(char *filename, int file) {
+char* read_file(char *filename) {
 	struct stat file_stat;	
 	stat(filename, &file_stat);
 	filesize = (long)file_stat.st_size; 
 	char *buffer;
 	buffer = (char*)malloc(sizeof(char)*filesize);
-	if(read(file, (void *)buffer, (size_t)filesize)==-1){
+	if(read(f, (void *)buffer, (size_t)filesize)==-1){
 		perror("read");
 		exit(EXIT_FAILURE);	
 	}		
-	char *contents = buffer;
+	char *content = buffer;
 	free(buffer);
-	return contents;
+	return content;
 }
 
-void write_file(char* content, int archive) {
-	/*if(write(archive, (void *)filesize, sizeof(int))==-1) {
+void write_file(char* content) {
+	/*if(write(a, (void *)filesize, sizeof(int))==-1) {
 		perror("write size");
 		exit(EXIT_FAILURE);	
 	}*/
-	if(write(archive, (void *)content, strlen(content))==-1) {
+	if(write(a, (void *)content, strlen(content))==-1) {
 		perror("write content");
 		exit(EXIT_FAILURE);	
 	}
@@ -83,8 +84,9 @@ void delete_older_from_archive() {
 	// TO DO
 }
 
-void add_in_archive(char *filename,int file, int archive) {
-	write_file(read_file(filename, file), archive);
+void add_in_archive(char *filename) {
+	char *temp = read_file(filename);
+	write_file(temp);
 }
 
 int file_is_modified(char *filename) {
@@ -124,25 +126,18 @@ void record_file(int delay, int number, char *filename, char *archivename) {
 	
 	else{*/
 	int fd;
-		fd=open(archivename,O_RDWR|O_CREAT,S_IRWXO);
-		if(fd==-1) {
-				perror("open archive with rights");
-				exit(EXIT_FAILURE);
-			}
-		close(fd);
-	
-	
-	
-	
+	fd=open(archivename,O_WRONLY|O_CREAT|O_TRUNC, S_IRUSR|S_IWUSR);
+	if(fd==-1) {
+		perror("open archive with rights");
+		exit(EXIT_FAILURE);
+	}
+	close(fd);
+
 	// Boucle infinie qui se lance toutes les "delay" secondes
 	while(TRUE) {
 		if(file_is_modified(filename)) {
 			// Ouverture de l'archive et du fichier a surveiller
-
-			int file;
-			int archive;
-			
-			archive=open(archivename, O_RDWR,S_IRWXO);
+			archive=open(archivename,O_WRONLY|O_CREAT, S_IRUSR|S_IWUSR);
 			file=open(filename, O_RDONLY);
 			
 			if(archive==-1) {
@@ -166,13 +161,13 @@ void record_file(int delay, int number, char *filename, char *archivename) {
 			
 			if(files_in_archive<=number) {
 				// Ajout du fichier dans l'archive
-				add_in_archive(filename, file, archive);
+				add_in_archive(filename);
 			}
 			else {
 				// Suppression du premier fichier de l'archive
 				delete_older_from_archive();
 				// Ajout du fichier dans l'archive
-				add_in_archive(filename, file, archive);
+				add_in_archive(filename);
 			}
 			
 			// Liberation des locks
